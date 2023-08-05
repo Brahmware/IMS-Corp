23a42e79c917e71348b0c6c317aefd817b1fdeaf
sanchayan721 <sanchayan.bhunia@hotmail.com>
2023-08-05 06:06:09 +0200

    new changes

---
+++ app/api/health/route.ts
@@ -0,0 +1,8 @@
+// pages/api/health.ts
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default (_req: NextApiRequest, res: NextApiResponse) => {
+  
+  res.status(200).json({ status: 'Healthy' });
+};
