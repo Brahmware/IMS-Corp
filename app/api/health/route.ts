c9938cf270286aab26c43f084ce3c6b5f5f6f790
sanchayan721 <sanchayan.bhunia@hotmail.com>
2023-08-08 06:33:05 +0200

    test

--- app/api/health/route.ts
+++ app/api/health/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
+// This code uses the GET method to return a 200 OK response.
+
 export async function GET(req: NextRequest) {
   return new NextResponse('OK', { status: 200 });
 };
